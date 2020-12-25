fa99dd11506f1854302117463da96d43fdeae50a
Ahmed Shakir <ahmed.shakir@supernovait.se>
2020-12-25 14:52:04 +0100

    Remove Spring REST Docs

--- src/test/java/com/example/user/controllers/UserControllerTest.java
+++ src/test/java/com/example/user/controllers/UserControllerTest.java
@@ -2,41 +2,27 @@ package com.example.user.controllers;
 
 import com.example.user.entities.User;
 import com.example.user.repositories.UserRepository;
+import com.example.user.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.constraints.ConstraintDescriptions;
-import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.StringUtils;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
+import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -46,156 +32,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @version 1.0
  * @since 2020-11-01
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-//@WebMvcTest(UserController.class)
-@ExtendWith(RestDocumentationExtension.class)
-@AutoConfigureRestDocs(uriPort = 7000)
+@WebMvcTest(UserController.class)
 @ActiveProfiles("test")
 public class UserControllerTest {
-    //@Autowired
+    @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
     @MockBean
     private UserRepository userRepository;
+    @MockBean
+    private UserService userService;
+    private User user;
 
     @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .apply(SecurityMockMvcConfigurers.springSecurity())
-                .build();
-    }
-
-    @Test
-    @WithMockUser(value = "admin", roles = {"ADMIN"})
-    void findAllUsers() throws Exception {
-        given(userRepository.findAll()).willReturn(List.of(getValidUser()));
-
-        mockMvc.perform(get("/api/v1/users")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("v1/users-get-all"));
-    }
-
-    @Test
-    @WithMockUser(value = "admin", roles = {"ADMIN"})
-    void findUserById() throws Exception {
-        given(userRepository.findById(any())).willReturn(Optional.of(getValidUser()));
-
-        mockMvc.perform(get("/api/v1/users/{id}", UUID.randomUUID().toString())
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andDo(document("v1/users-get-one",
-                        pathParameters(
-                                parameterWithName("id").description("UUID string of desired user to get.")
-                        ),
-                        responseFields(
-                                fieldWithPath("id").description("User ID"),
-                                fieldWithPath("firstname").description("Users firstname"),
-                                fieldWithPath("lastname").description("Users lastname"),
-                                fieldWithPath("birthday").description("Users birthday")/*.type(LocalDate.class)*/,
-                                fieldWithPath("mail").description("Users mail address"),
-                                fieldWithPath("phone").description("Users phone number"),
-                                fieldWithPath("username").description("Users username"),
-                                fieldWithPath("acl").description("Users authorities"),
-                                fieldWithPath("pet").description("Users pet")
-                        )));
-    }
-
-    @Test
-    @WithMockUser(value = "admin", roles = {"ADMIN"})
-    void saveUser() throws Exception {
-        User user = getValidUser();
-        user.setId(null);
-        String userJson = objectMapper.writeValueAsString(user);
-        userJson = userJson.replace("}", ",\"password\":\"" + user.getPassword() + "\"}");
-
-        given(userRepository.save(any())).willReturn(getValidUser());
-
-        UserControllerTest.ConstrainedFields fields = new UserControllerTest.ConstrainedFields(User.class);
-
-        mockMvc.perform(post("/api/v1/users")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(userJson))
-                .andExpect(status().isCreated())
-                .andDo(document("v1/users-new",
-                        requestFields(
-                                fields.withPath("id").ignored(),
-                                fields.withPath("firstname").description("Users firstname"),
-                                fields.withPath("lastname").description("Users lastname"),
-                                fields.withPath("birthday").description("Users birthday")/*.type(LocalDate.class)*/,
-                                fields.withPath("mail").description("Users mail address"),
-                                fields.withPath("phone").description("Users phone number"),
-                                fields.withPath("username").description("Users username"),
-                                fields.withPath("password").description("Users password"),
-                                fields.withPath("acl").description("Users authorities"),
-                                fields.withPath("pet").description("Users pet").optional()
-                        ),
-                        responseFields(
-                                fieldWithPath("id").description("User ID"),
-                                fieldWithPath("firstname").description("Users firstname"),
-                                fieldWithPath("lastname").description("Users lastname"),
-                                fieldWithPath("birthday").description("Users birthday")/*.type(LocalDate.class)*/,
-                                fieldWithPath("mail").description("Users mail address"),
-                                fieldWithPath("phone").description("Users phone number"),
-                                fieldWithPath("username").description("Users username"),
-                                fieldWithPath("acl").description("Users authorities"),
-                                fieldWithPath("pet").description("Users pet")
-                        )));
-    }
-
-    @Test
-    @WithMockUser(value = "admin", roles = {"ADMIN"})
-    void updateUser() throws Exception {
-        User user = getValidUser();
-        String userJson = objectMapper.writeValueAsString(user);
-        userJson = userJson.replace("}", ",\"password\":\"" + user.getPassword() + "\"}");
-        System.out.println("user:\n" + userJson);
-
-        UserControllerTest.ConstrainedFields fields = new UserControllerTest.ConstrainedFields(User.class);
-
-        given(userRepository.existsById(any())).willReturn(true);
-
-        mockMvc.perform(put("/api/v1/users/{id}", UUID.randomUUID().toString())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(userJson))
-                .andExpect(status().isNoContent())
-                .andDo(document("v1/users-update",
-                        pathParameters(
-                                parameterWithName("id").description("UUID string of desired user to update.")
-                        ),
-                        requestFields(
-                                fields.withPath("id").description("User ID"),
-                                fields.withPath("firstname").description("Users firstname"),
-                                fields.withPath("lastname").description("Users lastname"),
-                                fields.withPath("birthday").description("Users birthday"),
-                                fields.withPath("mail").description("Users mail address"),
-                                fields.withPath("phone").description("Users phone number"),
-                                fields.withPath("username").description("Users username"),
-                                fields.withPath("password").description("Users password"),
-                                fields.withPath("acl").description("Users authorities"),
-                                fields.withPath("pet").description("Users pet").optional()
-                        )));
-    }
-
-    @Test
-    @WithMockUser(value = "admin", roles = {"ADMIN"})
-    void deleteUser() throws Exception {
-        //System.out.println("Authorities: " + SecurityContextHolder.getContext().getAuthentication().getAuthorities());
-
-        given(userRepository.existsById(any())).willReturn(true);
-
-        mockMvc.perform(delete("/api/v1/users/{id}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent())
-                .andDo(document("v1/users-delete",
-                        pathParameters(
-                                parameterWithName("id").description("UUID string of desired user to delete.")
-                        )));
-    }
-
-    private User getValidUser() {
-        return User.builder()
+    public void setUp() {
+        user = User.builder()
                 .id(UUID.randomUUID().toString())
                 .firstname("Kalle")
                 .lastname("Karlsson")
@@ -208,15 +60,70 @@ public class UserControllerTest {
                 .build();
     }
 
-    private static class ConstrainedFields {
-        private final ConstraintDescriptions constraintDescriptions;
+    @Test
+    @WithMockUser(value = "admin", roles = {"ADMIN"})
+    void findAllUsers() throws Exception {
+        given(userService.findAll(null, false)).willReturn(List.of(user));
 
-        ConstrainedFields(Class<?> input) {
-            this.constraintDescriptions = new ConstraintDescriptions(input);
-        }
+        mockMvc.perform(get("/api/v1/users")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
-        private FieldDescriptor withPath(String path) {
-            return fieldWithPath(path).attributes(key("constraints").value(StringUtils.collectionToDelimitedString(this.constraintDescriptions.descriptionsForProperty(path), ". ")));
-        }
+    @Test
+    @WithMockUser(value = "admin", roles = {"ADMIN"})
+    void findUserById() throws Exception {
+        given(userService.findById(any())).willReturn(user);
+
+        mockMvc.perform(get("/api/v1/users/{id}", user.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(user.getId())))
+                .andExpect(jsonPath("$.username", is(user.getUsername())))
+                .andExpect(jsonPath("$.mail", is(user.getMail())));
+    }
+
+    @Test
+    @WithMockUser(value = "admin", roles = {"ADMIN"})
+    void saveUser() throws Exception {
+        String id = user.getId();
+        user.setId(null);
+        String userJson = objectMapper.writeValueAsString(user);
+        userJson = userJson.replace("}", ",\"password\":\"" + user.getPassword() + "\"}");
+        user.setId(id);
+
+        given(userService.save(any())).willReturn(user);
+
+        mockMvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(user.getId())))
+                .andExpect(jsonPath("$.username", is(user.getUsername())))
+                .andExpect(jsonPath("$.mail", is(user.getMail())));
+    }
+
+    @Test
+    @WithMockUser(value = "admin", roles = {"ADMIN"})
+    void updateUser() throws Exception {
+        String userJson = objectMapper.writeValueAsString(user);
+        userJson = userJson.replace("}", ",\"password\":\"" + user.getPassword() + "\"}");
+
+        given(userRepository.existsById(any())).willReturn(true);
+
+        mockMvc.perform(put("/api/v1/users/{id}", user.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithMockUser(value = "admin", roles = {"ADMIN"})
+    void deleteUser() throws Exception {
+        given(userRepository.existsById(any())).willReturn(true);
+
+        mockMvc.perform(delete("/api/v1/users/{id}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
     }
 }
